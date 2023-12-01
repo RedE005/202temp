38c540206aab705602a133ace3df8cb709b3e954
Shiva Ram Reddy Bethi <iamreddy005@gmail.com>
2023-11-30 19:32:48 -0800

    Update Seat.jsx - fixed Icon (Jatin commit)

--- client/src/components/Seat.jsx
+++ client/src/components/Seat.jsx
@@ -1,47 +1,44 @@
-import { CheckIcon } from '@heroicons/react/24/outline'
-import { memo, useState } from 'react'
-import { MdEventSeat } from "react-icons/md";
+import { CheckIcon } from '@heroicons/react/24/outline';
+import { memo, useState } from 'react';
 import { PiArmchairDuotone } from "react-icons/pi";
 
 const Seat = ({ seat, setSelectedSeats, selectable, selectedSeats, isAvailable, isLimitExceeded }) => {
-	const [isSelected, setIsSelected] = useState(false)
-	return (!isAvailable || (isLimitExceeded && !selectedSeats.includes(`${seat.row}${seat.number}`)))? (
-		<PiArmchairDuotone
-			title={`${seat.row}${seat.number}`}
-			className="flex h-8 w-8 cursor-not-allowed items-center justify-center transform scale -x-[-1]"
-		>
-			<div className="h-6 w-6 rounded-full bg-gray-500 drop-shadow-md"></div>
-		</PiArmchairDuotone>
-	) : isSelected ? (
-		<PiArmchairDuotone
-			title={`${seat.row}${seat.number}`}
-			className="flex h-8 w-8 items-center justify-center transform scale -x-[-1]"
-			onClick={() => {
-				setIsSelected(false)
-				setSelectedSeats((prev) => prev.filter((e) => e !== `${seat.row}${seat.number}`))
-				console.log(selectedSeats)
-			}}
-		>
-			<div className="flex h-6 w-6 items-center justify-center rounded-full bg-blue-500 drop-shadow-md">
-				<CheckIcon className="h-5 w-5 stroke-[3] text-white" />
-			</div>
-		</PiArmchairDuotone>
-	) : (
-		<PiArmchairDuotone
-			title={`${seat.row}${seat.number}`}
-			className={`flex h-8 w-8 items-center justify-center ${!selectable && 'cursor-not-allowed'} transform scale -x-[-1]`}
-			onClick={() => {
-				if (selectable) {
-					setIsSelected(true)
-					setSelectedSeats((prev) => [...prev, `${seat.row}${seat.number}`])
-					console.log(selectedSeats)
-				}
-			}}
-		>
-			<div className="h-6 w-6 rounded-full bg-white drop-shadow-md"></div>
-		</PiArmchairDuotone>
-		
-	)
-}
+  const [isSelected, setIsSelected] = useState(false);
 
-export default memo(Seat)
+  const handleSeatClick = () => {
+    if (!selectable || !isAvailable || (isLimitExceeded && !selectedSeats.includes(`${seat.row}${seat.number}`))) return;
+    setIsSelected(!isSelected);
+    setSelectedSeats((prevSelectedSeats) => {
+      return isSelected
+        ? prevSelectedSeats.filter((s) => s !== `${seat.row}${seat.number}`)
+        : [...prevSelectedSeats, `${seat.row}${seat.number}`];
+    });
+  };
+
+  return (
+    <PiArmchairDuotone
+      title={`${seat.row}${seat.number}`}
+      className={`flex items-center justify-center ${!selectable || !isAvailable ? 'cursor-not-allowed opacity-50' : 'cursor-pointer'} transform scale-x-[-1] `}
+      onClick={handleSeatClick}
+      disabled={!isAvailable}
+      style={{
+        height: '32px',
+        width: '32px',
+        backgroundColor: isSelected ? 'blue' : 'transparent',
+        borderColor: isAvailable ? 'blue' : 'grey',
+        borderWidth: '2px',
+        borderStyle: 'solid',
+        borderRadius: '50%',
+        margin: '12px',
+        position: 'relative',
+        boxShadow: isSelected ? '0 0 10px blue' : 'none',
+      }}
+    >
+      {isSelected && (
+        <CheckIcon className="absolute h-5 w-5 text-white" />
+      )}
+    </PiArmchairDuotone>
+  );
+};
+
+export default memo(Seat);
